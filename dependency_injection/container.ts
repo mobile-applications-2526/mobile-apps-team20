09ce11991cdbe034034280877a2049817961259d
juan <juanjuangmo@gmail.com>
2025-11-11 14:29:29 +0100

    Refactor: Update dependency injection container to include authentication data sources and repositories

--- dependency_injection/container.ts
+++ dependency_injection/container.ts
@@ -1,5 +1,7 @@
-import { EventDataSourceImpl } from "@/domain/infrastructure/datasources/event_datasource_impl";
-import { EventRepositoryImpl } from "@/domain/infrastructure/repositories/event_repository_impl";
+import { AuthDataSourceImpl } from "@/domain/infrastructure/datasources/auth/auth_datasource_impl";
+import { EventDataSourceImpl } from "@/domain/infrastructure/datasources/events/event_datasource_impl";
+import { AuthRepositoryImpl } from "@/domain/infrastructure/repositories/auth/auth_repository_impl";
+import { EventRepositoryImpl } from "@/domain/infrastructure/repositories/events/event_repository_impl";
 import { ApiServiceImpl } from "@/domain/services_impl/api_service_impl";
 
 // Container for dependency injection
@@ -7,8 +9,13 @@ const apiService = new ApiServiceImpl();
 const eventDataSource = new EventDataSourceImpl(apiService);
 const eventRepository = new EventRepositoryImpl(eventDataSource);
 
+// Auth
+const authDataSource = new AuthDataSourceImpl(apiService);
+const authRepository = new AuthRepositoryImpl(authDataSource);
+
 export const container = {
   apiService,
   eventDataSource,
   eventRepository,
+  authRepository
 };
